2954bb27b643e065c455196b5e95acb1a05c4c2a
vishvish2 <vishnubarath909@gmail.com>
2026-05-29 10:47:57 +0100

    Created testbench for program counter module

---
+++ README.md
@@ -0,0 +1,23 @@
+# Command to compile testbench code
+`iverilog -g 2012 -o <module name/testbench module name>.vvp <module name/testbench module name>.sv`
+
+e.g.
+`iverilog -g 2012 -o program_counter/program_counter_tb.vvp program_counter/program_counter_tb.sv`
+
+This will create a .vvp file for the module
+
+# Command for viewing output of testbench compilation
+`vvp <module name/testbench module name>.vvp`
+
+e.g.
+`vvp program_counter/program_counter_tb.vvp`
+
+This will create a .vcd file for the module
+
+# Command for viewing waveforms of variables from testbench
+`gtkwave <module name/testbench module name>.vcd`
+
+e.g.
+`gtkwave program_counter/program_counter_tb.vcd`
+
+
---
+++ program_counter/program_counter_tb.sv
@@ -0,0 +1,53 @@
+`timescale 1ns/1ps                                      // Define time units
+`include "program_counter/program_counter.sv"           // Include pc module
+
+module program_counter_tb;
+
+logic [31:0] pc, pcnext;
+logic clk, reset;                                       // Variable declaration
+program_counter dut (pc, pcnext, clk, reset);           // Instantiate the module under test
+
+initial begin
+    $dumpfile("program_counter/program_counter_tb.vcd");// Dump variable changes in the vcd file
+    $dumpvars(0, program_counter_tb);                   // Specifies which variables to dump in the vcd file
+    // Initialise signals
+    reset = 1;
+    pcnext = 32'h00000000;
+
+    #10;
+    reset = 0;
+
+    #10;
+    pcnext = 32'h00000004;
+
+    #10;
+    pcnext = 32'h00000008;
+
+    #10;
+    pcnext = 32'h0000000C;
+
+    #10;
+    pcnext = 32'h00000010;
+
+    #10;
+    reset = 1;  // Test reset again
+    pcnext = 32'h00000014;
+
+    #10;
+    reset = 0;
+    pcnext = 32'h00000020;
+
+    #20;
+
+    $finish;
+end
+
+initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+end
+
+initial begin                       
+    $monitor("t = %3d, pc = %h, pcnext = %h, clk = %b, reset = %b \n", $time, pc, pcnext, clk, reset);
+end                                 // Print variable values on screen
+endmodule
