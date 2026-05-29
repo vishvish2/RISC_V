ec7769144de55b43a4f4ca3925ab7e8ec8c0635b
vishvish2 <vishnubarath909@gmail.com>
2026-05-29 11:45:01 +0100

    Created immediate value extension module and a test bench for it

---
+++ extend/extend_tb.sv
@@ -0,0 +1,27 @@
+`timescale 1ns/1ps
+`include "extend/extend.sv"
+
+module extend_tb;
+
+logic [31:0] immext, instr;
+logic [2:0] immsrc;
+extend dut (immext, immsrc, instr);
+
+initial begin
+    $dumpfile("extend/extend_tb.vcd");
+    $dumpvars(0, extend_tb);
+    immsrc = 3'b000; instr = 32'hB4B5CAD7; #10
+    immsrc = 3'b001; instr = 32'h6B95BCA9; #10
+    immsrc = 3'b010; instr = 32'hCCD672B6; #10
+    immsrc = 3'b011; instr = 32'h3D2DE34A; #10
+    immsrc = 3'b100; instr = 32'hE3A6B935; #10
+    
+    $finish;
+end
+
+initial begin
+    $monitor("t = %3d, immsrc = %b, instr = %b, immext = %b \n",
+    $time, immsrc, instr, immext);
+end
+
+endmodule
